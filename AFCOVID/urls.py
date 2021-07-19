34c2d183b048a6c65b0275ec340ac45a73409699
Pongthep Janseana <pongthep@rtaf.mi.th>
2021-07-19 20:44:03 +0700

    190764

--- AFCOVID/urls.py
+++ AFCOVID/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
    
     path('', include('Patient.urls')),
 
-]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
