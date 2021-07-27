7350b37c0ff5b62e798511c6a79758e31a13f866
Pongthep Janseana <pongthep@rtaf.mi.th>
2021-07-28 05:34:40 +0700

    640728:0534 - change wrong word

--- Corona3/views.py
+++ Corona3/views.py
@@ -2,6 +2,7 @@
 from django.forms.forms import Form
 from django.views.generic import CreateView
 from django.contrib import messages
+from django.shortcuts import redirect
 
 from .models import Corona3
 from .forms import Corona3BasicDataForm
@@ -17,6 +18,11 @@ class Corona3BasicFormAddNewView(CreateView):
     def form_invalid(self, form):
         print(self.request, form.errors)
         return super().form_invalid(form)
+    
+    def post(self, request):
+        super(Corona3BasicFormAddNewView, self).post(request)
+        messages.success(request,"บันทึกข้อมูลเรียบร้อย")
+        return redirect('/CRN3/BasicData/')
 
 
 
